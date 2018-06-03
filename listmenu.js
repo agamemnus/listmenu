365387d15222e0f9cd5173b4f32c3a10ef135246
agamemnus <agamemnus@users.noreply.github.com>
2018-06-03 17:00:46 -0400

    Update listmenu.js

--- listmenu.js
+++ listmenu.js
@@ -30,23 +30,23 @@ function ListMenu (settings) {
  var unhighlight = listmenu.unhighlight = function (obj) {
   if (typeof obj.listmenu_settings == "undefined") obj.listmenu_settings = {}
   if ((typeof obj.listmenu_settings.selected != "undefined") && (obj.listmenu_settings.selected == true)) return
-  obj.classList.remove (listmenu.css_lit_class)
+  obj.classList.remove(listmenu.css_lit_class)
  }
  var set_selected = listmenu.set_selected = function (obj) {
   if (typeof obj.listmenu_settings == "undefined") obj.listmenu_settings = {}
   if (obj.listmenu_settings.unselectable) return
   obj.listmenu_settings.selected = true
-  highlight (obj)
+  highlight(obj)
  }
  var clear_selected = listmenu.clear_selected = function (obj) {
   if (typeof obj.listmenu_settings == "undefined") obj.listmenu_settings = {}
   obj.listmenu_settings.selected = false
-  unhighlight (obj)
+  unhighlight(obj)
  }
  var toggle_selected = listmenu.toggle_selected = function (obj) {
   if (typeof obj.listmenu_settings == "undefined") obj.listmenu_settings = {}
   obj.listmenu_settings.selected = !obj.listmenu_settings.selected
-  if (obj.listmenu_settings.selected == true) {highlight (obj)} else {unhighlight (obj)}
+  if (obj.listmenu_settings.selected == true) {highlight(obj)} else {unhighlight(obj)}
  }
  var queued_events = []
  
@@ -66,21 +66,20 @@ function ListMenu (settings) {
    function body_touchstart_listener (evt) {
     if ((obj instanceof HTMLElement == false) || (is_attached(obj) == false)) document.body.removeEventListener ('mousedown', body_touchstart_listener)
     if ((obj == evt.target) || (obj.contains(evt.target))) return
-    evt.stopPropagation ()
+    evt.stopPropagation()
     if (obj.listmenu_settings.is_open == false) return
     // Close the menu.
-    listmenu.unhighlight (obj)
-    listmenu.close (obj)
+    listmenu.unhighlight(obj)
+    listmenu.close(obj)
    }
   }
   function recurse_gen (obj) {
    if (obj.tagName.toLowerCase() == listmenu.container_tag) {var container = obj} else {var container = obj.querySelector (listmenu.container_tag)}
    if (typeof container.listmenu_settings == "undefined") container.listmenu_settings = {}
    var is_root = (typeof container.listmenu_settings.parent == "undefined")
-   var list_items = container.listmenu_settings.children = get_elements_until (container, listmenu.list_item_tag, listmenu.container_tag)
+   var list_items = container.listmenu_settings.children = get_elements_until(container, listmenu.list_item_tag, listmenu.container_tag)
    if (!is_root) container.style.display = "none"
-   for (var i = 0, curlen = list_items.length; i < curlen; i++) {
-    var list_item = list_items[i]
+   list_items.forEach(function (list_item) {
     if (typeof list_item.listmenu_settings == "undefined") list_item.listmenu_settings = {}
     if (typeof list_item.listmenu_settings.is_open            == "undefined") list_item.listmenu_settings.is_open           = false
     if (typeof list_item.listmenu_settings.hide_on_left_click == "undefined") list_item.listmenu_settings.hide_on_left_click = settings_hide_on_left_click
@@ -90,35 +89,35 @@ function ListMenu (settings) {
      list_item.listmenu_settings.children = [child_child]
      if (typeof child_child.listmenu_settings == "undefined") child_child.listmenu_settings = {}
      child_child.listmenu_settings.parent = list_item
-     recurse_gen (child_child)
+     recurse_gen(child_child)
     } else {
      list_item.listmenu_settings.children = []
     }
     // Add mouseover, mouseout, and mouseup events on the list_item elements.
     if (settings_touch_events == true) {
-     list_item.addEventListener ('touchstlart', show_event)
+     list_item.addEventListener('touchstart', show_event)
     } else {
      if ((settings_hide_on_click == true) && (settings_hide_on_click == true)) {
-      list_item.addEventListener ('mouseup', show_or_hide_event)
+      list_item.addEventListener('mouseup', show_or_hide_event)
      } else {
-      if (settings_hide_on_click == true) list_item.addEventListener ('mouseup', function (evt) {hide_event (evt, true, true)})
-      if (settings_show_on_click == true) list_item.addEventListener ('mouseup', function (evt) {show_event (evt, true, true)})
+      if (settings_hide_on_click == true) list_item.addEventListener('mouseup', function (evt) {hide_event (evt, true, true)})
+      if (settings_show_on_click == true) list_item.addEventListener('mouseup', function (evt) {show_event (evt, true, true)})
      }
-     if (settings_hide_on_mouseover == true) list_item.addEventListener ('mouseout' , hide_event)
-     if (settings_show_on_mouseover == true) list_item.addEventListener ('mouseover', show_event)
+     if (settings_hide_on_mouseover == true) list_item.addEventListener('mouseout' , hide_event)
+     if (settings_show_on_mouseover == true) list_item.addEventListener('mouseover', show_event)
     }
-    list_item.addEventListener ((settings_touch_events == true) ? 'touchstart' : 'mouseup', click_event)
-   }
+    list_item.addEventListener((settings_touch_events == true) ? 'touchstart' : 'mouseup', click_event)
+   })
   }
  }
  
  function clear_queued_events () {
-  for (var i = 0, curlen = queued_events.length; i < curlen; i++) {clearTimeout (queued_events[i])}
+  queued_events.forEach(function (queued_event) {clearTimeout(queued_event)})
   queued_events = []
  }
  
  function show_or_hide_event (evt) {
-  if (hide_event (evt, true, true) == false) show_event (evt, true, true)
+  if (hide_event(evt, true, true) == false) show_event(evt, true, true)
  }
  
  function hide_event (evt, hide_on_click, is_immediate) {
@@ -135,15 +134,10 @@ function ListMenu (settings) {
   if ((listmenu.hide_stick_delay == 0) || (is_immediate == true)) {return hide_event_cont()} else {queued_events.push (setTimeout (hide_event_cont, listmenu.hide_stick_delay))}
   function hide_event_cont () {
    // If root menu, hide children's children instead of hiding self.
-   for (var i = 0, curlen = container_list.length; i < curlen; i++) {
-    var container = container_list[i]
-    if (typeof container.listmenu_settings.parent == "undefined") {
-     var hide_list = hide_grandchildren (container, 0)
-    } else {
-     var hide_list = hide_children (container.listmenu_settings.parent)
-    }
-   }
-   begin_process (hide_list, 'hide')
+   container_list.forEach (function (container) {
+    var hide_list = container.listmenu_settings.parent ? hide_children(container.listmenu_settings.parent) : hide_grandchildren(container, 0)
+	begin_process (hide_list, 'hide')
+   })
    return true
   }
  }
@@ -159,56 +153,52 @@ function ListMenu (settings) {
   function show_event_cont () {
    if (show_on_click && (list_item.listmenu_settings.is_open == true)) return false
    // Show own children.
-   var show_list = show_children (list_item)
+   var show_list = show_children(list_item)
    
    // Hide siblings' children.
-   var siblings = list_item.listmenu_settings.parent.listmenu_settings.children
    var hide_list = []
-   for (var i = 0, curlen = siblings.length; i < curlen; i++) {
-    if (siblings[i] != list_item) hide_list = hide_list.concat (hide_children (siblings[i]))
-   }
+   list_item.listmenu_settings.parent.listmenu_settings.children.forEach (function (sibling) {
+    if (sibling != list_item) hide_list = hide_list.concat(hide_children(sibling))
+   })
    
    begin_process (show_list, 'show')
    begin_process (hide_list, 'hide')
    return true
   }
  }
-  
+ 
  function show_children (list_item, record) {
   var display_type = (typeof list_item.listmenu_settings.parent != "undefined") ? "block" :  "inline-block"
   var children = list_item.listmenu_settings.children
   return children
  }
-  
+ 
  function hide_children (list_item, level) {
   var children = list_item.listmenu_settings.children
   var descendant_list = children
-  for (var i = 0, curlen = children.length; i < curlen; i++) {
-   var container = children[i]
-   if (typeof container.listmenu_settings.children != "undefined") descendant_list = descendant_list.concat (hide_grandchildren (container, level))
-  }
+  children.forEach (function (container) {
+   if (typeof container.listmenu_settings.children != "undefined") descendant_list = descendant_list.concat(hide_grandchildren(container, level))
+  })
   return descendant_list
  }
  
  function hide_grandchildren (container, level) {
-  if (typeof level != "undefined") {var new_level = level + 1} else {var new_level = undefined}
+  var new_level = (typeof level != "undefined") ? level + 1 : undefined
   var children = container.listmenu_settings.children
   var descendant_list = []
-  for (var i = 0, curlen = children.length; i < curlen; i++) {
-   var list_item = children[i]
+  children.forEach (function (list_item) {
    if ((typeof level == "undefined") || (level > 0)) listmenu.unhighlight (list_item)
    descendant_list = descendant_list.concat (hide_children (list_item, new_level))
-  }
+  })
   return descendant_list
  }
  
  // Run the show or hide process function.
  function begin_process (showhide_list, showhide, options) {
-  for (var i = 0, curlen = showhide_list.length; i < curlen; i++) {
-   if (typeof showhide_list[i].listmenu_settings.parent != "undefined") {
-    showhide_list[i].listmenu_settings.parent.listmenu_settings.is_open = (showhide == 'show') ? true : false
-   }
-  }
+  showhide_list.forEach (function (entry) {
+   if (typeof entry.listmenu_settings.parent == "undefined") return
+   entry.listmenu_settings.parent.listmenu_settings.is_open = (showhide == 'show') ? true : false
+  })
   if ((typeof listmenu.animate) == "undefined") return
   if (typeof options == "undefined") options = {}
   // If options.instant is set to true, show/hide immediately. If that's not set and true, try the listmenu default delay.
@@ -217,7 +207,8 @@ function ListMenu (settings) {
   // If an element is included in this showhide_list, remove it from its current showhide_list, if it exists.
   // Further, make the element aware what showhide_list it is in.
   var showhide_list_new = []
-  var j = 0; for (var i = 0, curlen = showhide_list.length; i < curlen; i++) {
+  var j = 0
+  showhide_list.forEach(function (obj) {
    var obj = showhide_list[i]
    if (typeof obj.listmenu_settings == "undefined") obj.listmenu_settings = {}
    if (typeof obj.listmenu_settings.showhide_list != "undefined") {
@@ -225,23 +216,23 @@ function ListMenu (settings) {
     showhide_list_temp[obj.listmenu_settings.showhide_list_index] = undefined
    }
    // Don't add already hidden elements if showhide is "hide" and display is "none".
-   if ((showhide == 'hide') && (obj.style.display == 'none')) continue
+   if ((showhide == 'hide') && (obj.style.display == 'none')) return
    obj.listmenu_settings.showhide_list       = showhide_list_new
    obj.listmenu_settings.showhide_list_index = j
    j += 1
-   showhide_list_new.push (obj)
-  }
+   showhide_list_new.push(obj)
+  })
   showhide_list = showhide_list_new
   var max_steps = (listmenu[showhide + '_process_timeout_time'] == 0) ? 1 : showhide_delay / listmenu[showhide + '_process_timeout_time'] + 1
   var current_step = 0
-  showhide_process_internal ()
+  showhide_process_internal()
   function showhide_process_internal () {
    if (current_step > max_steps) return
    var current_counter = current_step / max_steps
    if (showhide == 'hide') current_counter = 1 - current_counter
-   listmenu.animate (showhide_list, current_counter, showhide)
+   listmenu.animate(showhide_list, current_counter, showhide)
    current_step += 1
-   setTimeout (showhide_process_internal, settings[showhide + '_process_timeout_time'])
+   setTimeout(showhide_process_internal, settings[showhide + '_process_timeout_time'])
   }
  }
  
@@ -250,40 +241,38 @@ function ListMenu (settings) {
   if (('which' in evt && evt.which == 3) || ('button' in evt && evt.button == 3)) return
   var list_item = evt.currentTarget
   if (list_item.listmenu_settings.children.length != 0) return
-  var siblings = list_item.listmenu_settings.parent.listmenu_settings.children
-  for (var i = 0, curlen = siblings.length; i < curlen; i++) {
-   if (siblings[i] != list_item) clear_selected (siblings[i])
-  }
-  set_selected (list_item)
+  list_item.listmenu_settings.parent.listmenu_settings.children.forEach(function (sibling) {
+   if (sibling != list_item) clear_selected(sibling)
+  })
+  set_selected(list_item)
   
   // Close the menu.
-  var hide_list = hide_ancestors_and_self (list_item.listmenu_settings.parent, [])
-  begin_process (hide_list, 'hide')
+  var hide_list = hide_ancestors_and_self(list_item.listmenu_settings.parent, [])
+  begin_process(hide_list, 'hide')
  }
  
  function hide_ancestors_and_self (container, hide_list) {
   if (typeof container.listmenu_settings.parent == "undefined") return hide_list
-  hide_list.push (container)
-  var container = container.listmenu_settings.parent.listmenu_settings.parent
+  hide_list.push(container)
   // Only modify the container node.
-  return hide_ancestors_and_self (container, hide_list)
+  return hide_ancestors_and_self(container.listmenu_settings.parent.listmenu_settings.parent, hide_list)
  }
  
- listmenu.close = function (container, options) {begin_process (hide_grandchildren (container, 0), 'hide', options)}
- listmenu.open  = function (container, options) {begin_process (hide_grandchildren (container)   , 'show', options)}
+ listmenu.close = function (container, options) {begin_process(hide_grandchildren(container, 0), 'hide', options)}
+ listmenu.open  = function (container, options) {begin_process(hide_grandchildren(container)   , 'show', options)}
  
  function get_elements_until (parent, tagname_to_search_for, tagname_to_stop_at) {
   var element_list = []
-  check_children (parent)
+  check_children(parent)
   function check_children (parent) {
    var children = parent.childNodes
-   for (var i = 0, curlen = children.length; i < curlen; i++) {
-    var child = children[i], tagname = child.tagName
-    if (typeof tagname == "undefined") continue
-    tagname = tagname.toLowerCase ()
-    if (tagname == tagname_to_search_for) element_list.push (child)
-    if (tagname != tagname_to_stop_at) check_children (child)
-   }
+   parent.childNodes.forEach(function (child) {
+    var tagname = child.tagName
+    if (typeof tagname == "undefined") return
+    tagname = tagname.toLowerCase()
+    if (tagname == tagname_to_search_for) element_list.push(child)
+    if (tagname != tagname_to_stop_at) check_children(child)
+   })
   }
   return element_list
  }
@@ -300,14 +289,10 @@ function ListMenu (settings) {
 }
 
 ListMenu.anim_fade = function (obj_list, counter, showhide) {
- for (var i = 0, curlen = obj_list.length; i < curlen; i++) {
-  var obj = obj_list[i]
-  if (typeof obj == "undefined") continue
-  if (showhide == 'show') {
-   if ((counter == 0) && (obj.style.display == "none")) {obj.style.opacity = 0; obj.style.display = "block"}
-   if (obj.style.opacity <= counter) obj.style.opacity = counter
-  } else {
-   if  (counter == 0) obj.style.display = "none"
-  }
- }
+ obj_list.forEach (function (obj) {
+  if (typeof obj == "undefined") return
+  if (showhide != "show") {if (counter == 0) obj.style.display = "none"; return}
+  if ((counter == 0) && (obj.style.display == "none")) {obj.style.opacity = 0; obj.style.display = "block"}
+  if (obj.style.opacity <= counter) obj.style.opacity = counter
+ })
 }
